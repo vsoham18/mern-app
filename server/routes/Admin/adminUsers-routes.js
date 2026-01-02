2fd75d3a31904406467956add82087646c9cb2ff
Soham Ghosh <ghoshsoham173@gmail.com>
2026-01-03 01:29:21 +0530

    add the frontend route

--- server/routes/Admin/adminUsers-routes.js
+++ server/routes/Admin/adminUsers-routes.js
@@ -2,13 +2,12 @@ import { Router } from "express";
 import { deleteUserController, editUserController, getAllUsers, getUserByIdController } from "../../controllers/Admin/admin-users-controllers.js";
 import { authorize } from "../../middlewares/auth-middleware.js";
 import { adminCheck } from "../../middlewares/admin-middleware.js";
-import { validate } from "../../middlewares/validate-middleware.js";
-import { signupSchema } from "../../validators/auth-validator.js";
+
 
 const userRouter = Router();
 
 userRouter.get("/users",authorize,adminCheck, getAllUsers);
 userRouter.delete("/users/delete/:Id",authorize,adminCheck, deleteUserController);
-userRouter.patch("/users/edit/:Id",authorize,adminCheck,validate(signupSchema), editUserController);
+userRouter.patch("/users/edit/:Id",authorize,adminCheck, editUserController);
 userRouter.get("/users/:Id",authorize,adminCheck, getUserByIdController);
 export default userRouter;  
